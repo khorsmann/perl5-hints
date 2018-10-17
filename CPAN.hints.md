197be12105788cac08005fb30a958f9c3ed33fe8
khorsmann <khorsmann@gmail.com>
2018-10-17 11:42:05 +0200

    add some CPAN hints and more array examples of basic usage

---
+++ CPAN.hints.md
@@ -0,0 +1,8 @@
+# CPAN hints
+
+## Force install of a Module
+anyhow if test fails (maybe networking is disabled or restricted)<br/>
+
+```shell
+cpan -fi REST::Client
+```
--- array.pl
+++ array.pl
@@ -2,22 +2,46 @@
 use v5.14;                              # use and enable features since 5.14
 use Data::Dumper;
 
+say "Setup an empty array";
+say 'my @array;';
+
 my @array;
+
+say "Fill in Values into the existing array";
+my $msg = <<'EOF';
+    $array[0] = "Chuck";
+    $array[1] = "Tim";
+    $array[2] = "Arnold";
+EOF
+
+say $msg;
+
 $array[0] = "Chuck";
 $array[1] = "Tim";
 $array[2] = "Arnold";
 
+say "##############################################";
+say "Iterate over an array";
+my $msg = <<'EOF';
+
+    for my $user (@array) {
+        say "For-Loop User: $user";
+    }
+EOF
+say $msg;
+
 for my $user (@array) {
     say "For-Loop User: $user";
 }
-say "";
 
+say "##############################################";
 say "or you can setup an array in one-line";
 say '@array = ("Sonja", "Rose", "Vicky", "Florence");';
 @array = ("Sonja", "Rose", "Vicky", "Florence");
 say "";
 
-say "copy position 1 to End of Array to new array";
+say "##############################################";
+say 'copy from position 1 to end of array to new array @names';
 say '@names = @array[ 1 .. $#array ];';
 say "";
 
@@ -26,6 +50,23 @@ foreach my $i (0 .. $#names) {
     say "$i - $names[$i]";
 }
 
+say "##############################################";
 say "array-end-pos: $#array";
 say "last array element: $array[-1]";
 print Dumper \@names;
+
+say "##############################################";
+say "replace an explicit position in array";
+my $msg = <<'EOF';
+
+    $array[2] = "second entry";
+    $array[0] = "first entry";
+EOF
+say $msg;
+print Dumper \@array;
+
+say "##############################################";
+say "insert something into the middle or somewhere";
+splice @array, 2, 0, "Splice something at Index number 2";
+print Dumper \@array;
+
