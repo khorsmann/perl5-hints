08f0918579044281991c24ba6a7da7cc67ba3f85
khorsmann <khorsmann@gmail.com>
2018-11-02 15:20:31 +0100

    add feature use

--- example1.pl
+++ example1.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 use v5.14;                              # use and enable features since 5.14
+use feature qw{ say  };                 # or enable say from new features
 
 print "\$someinput: ";                  # 'print' without newline
 chop(my $someinput=<STDIN>);            # initialise skalar '$someinput' from '<STDIN>'
